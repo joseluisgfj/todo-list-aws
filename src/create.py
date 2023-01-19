095be0e158ada849e86049c3f78501be1be37d50
joseluisgfj <122015613+joseluisgfj@users.noreply.github.com>
2023-01-19 17:45:58 +0100

    Update create.py

--- src/create.py
+++ src/create.py
@@ -7,20 +7,25 @@ import todoList
 def create(event, context):
     # Comentario JLGF: Se obtiene el texto introducido en el programa
     data = json.loads(event['body'])
-    # Comentario JLGF: Si no se introdujo texto en el programa, se escribe un error en el log y se lanza una excepción
+    # Comentario JLGF: Si no se introdujo texto en el programa, 
+    #               se escribe un error en el log y se lanza una excepción
     if 'text' not in data:
         # Comentario JLGF: Con la función logging.error se escribe en el log un error
         logging.error("Validation failed")
-        # Comentario JLGF:Se lanza una excepción con raise, y entre "" se escribe la Información de la excepción
+        # Comentario JLGF:Se lanza una excepción con raise, y entre "" 
+        #                  se escribe la Información de la excepción
         raise Exception("Couldn't create the todo item.")
-    # Comentario JLGF: Si se introdujo información valida en el campo, se procede a intrucirla en el programa mediante la funcion "todoList.put_item"
+    # Comentario JLGF: Si se introdujo información valida en el campo, 
+    #               se procede a intrucirla en el programa mediante la funcion "todoList.put_item"
     item = todoList.put_item(data['text'])
     # create a response
-    # Comentario JLGF: Se retorna una respuesta 200, que es equivalente a que el proceso a ido correctamente.
+    # Comentario JLGF: Se retorna una respuesta 200, 
+    #                  que es equivalente a que el proceso a ido correctamente.
     response = {
         "statusCode": 200,
         "body": json.dumps(item)
-        # Comentario JLGF:json.dumps, Este método permite convertir un objeto python en un objeto JSON serializado.
+        # Comentario JLGF:json.dumps, Este método permite convertir 
+        #               un objeto python en un objeto JSON serializado.
     }
     # Comentario JLGF: Se retorna una respuesta 200
     return response
