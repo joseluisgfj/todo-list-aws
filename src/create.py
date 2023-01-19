6a3c77c9bb893f9dcb46c764a185ba7c91a38f09
joseluisgfj <122015613+joseluisgfj@users.noreply.github.com>
2023-01-19 17:53:18 +0100

    Update create.py

--- src/create.py
+++ src/create.py
@@ -17,7 +17,8 @@ def create(event, context):
         # se escribe la Información de la excepción
         raise Exception("Couldn't create the todo item.")
     # Comentario JLGF: Si se introdujo información valida en el campo
-    # se procede a intrucirla en el programa mediante la funcion todoList.put_item
+    # se procede a intrucirla en el programa mediante 
+    # la funcion todoList.put_item
     item = todoList.put_item(data['text'])
     # create a response
     # Comentario JLGF: Se retorna una respuesta 200
