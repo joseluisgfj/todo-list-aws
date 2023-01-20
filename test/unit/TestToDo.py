a2cb2456350402dd66e156e42b4f9e42d92b63fc
joseluisgfj <122015613+joseluisgfj@users.noreply.github.com>
2023-01-20 10:59:51 +0100

    Update TestToDo.py

--- test/unit/TestToDo.py
+++ test/unit/TestToDo.py
@@ -119,6 +119,13 @@ class TestDatabaseFunctions(unittest.TestCase):
         self.assertTrue(result[0]['text'] == self.text)
         print ('End: test_list_todo')
 
+    def test_list_todo_error(self):
+        print ('---------------------')
+        print ('Start: test_list_todo_error')
+        from src.todoList import get_items
+        # Testing file functions
+        self.assertRaises(TypeError, get_items("", self.dynamodb))
+        print ('End: test_list_todo_error')
 
     def test_update_todo(self):
         print ('---------------------')
