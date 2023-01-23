98b77b78b21340b9079b55c7c03ffabfaa696cfc
joseluisgfj <122015613+joseluisgfj@users.noreply.github.com>
2023-01-23 11:07:30 +0100

    Update TestToDo.py

--- test/unit/TestToDo.py
+++ test/unit/TestToDo.py
@@ -253,7 +253,7 @@ class TestDatabaseFunctions(unittest.TestCase):
         print ('Start: test_delete_todo_error2')
         from src.todoList import delete_item
         # Testing file functions
-        self.assertRaises(TypeError, delete_item(-1, self.dynamodb))
+        self.assertRaises(TypeError, delete_item(-1, ""))
         print ('End: test_delete_todo_error')
 
 if __name__ == '__main__':
