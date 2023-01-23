398054c86832a2c4eb0091d26f9690c42114e0c9
joseluisgfj <122015613+joseluisgfj@users.noreply.github.com>
2023-01-23 11:46:13 +0100

    Update TestToDo.py

--- test/unit/TestToDo.py
+++ test/unit/TestToDo.py
@@ -33,7 +33,16 @@ class TestDatabaseFunctions(unittest.TestCase):
         self.table = create_todo_table(self.dynamodb)
         #self.table_local = create_todo_table()
         print ('End: setUp')
+        
+        
+   def setUp_error(self):
+        print ('---------------------')
+        print ('Start: setUp_error')
+        table.table_status = 'ERROR'
+        self.table_local = create_todo_table(-1)
+        print ('End: setUp_error')
 
+        
     def tearDown(self):
         print ('---------------------')
         print ('Start: tearDown')
