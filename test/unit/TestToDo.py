9db88a070aa4c249899a3a9404dfaa10b85f901a
joseluisgfj <122015613+joseluisgfj@users.noreply.github.com>
2023-01-23 11:17:13 +0100

    Update TestToDo.py

--- test/unit/TestToDo.py
+++ test/unit/TestToDo.py
@@ -36,29 +36,9 @@ class TestDatabaseFunctions(unittest.TestCase):
 
     def setUp_error(self):
         print ('---------------------')
-        print ('Start: setUp')
-        warnings.filterwarnings(
-            "ignore",
-            category=ResourceWarning,
-            message="unclosed.*<socket.socket.*>")
-        warnings.filterwarnings(
-            "ignore",
-            category=DeprecationWarning,
-            message="callable is None.*")
-        warnings.filterwarnings(
-            "ignore",
-            category=DeprecationWarning,
-            message="Using or importing.*")
-        """Create the mock database and table"""
-        self.dynamodb = boto3.resource('dynamodb', region_name='us-east-1')
-        self.is_local = 'true'
-        self.uuid = "123e4567-e89b-12d3-a456-426614174000"
-        self.text = "Aprender DevOps y Cloud en la UNIR"
-
-        from src.todoList import create_todo_table
-        # self.table = create_todo_table(self.dynamodb)
-        self.table_local = create_todo_table()
-        print ('End: setUp')
+        print ('Start: setUp_error')
+        self.table_local = create_todo_table(-1)
+        print ('End: setUp_error')
         
     def tearDown(self):
         print ('---------------------')
